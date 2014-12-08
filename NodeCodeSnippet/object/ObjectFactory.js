798ad9ebad9cb8ecef972a64eff7d07a135799ac
Prabhash Rathore <prabhashrathore@gmail.com>
2014-12-07 20:16:34 -0800

    playing around with object construction in js

--- NodeCodeSnippet/object/ObjectFactory.js
+++ NodeCodeSnippet/object/ObjectFactory.js
@@ -14,5 +14,17 @@ var a = Person instanceof Function;
 var b = Person instanceof Object;
 
 console.log("is Person instance of Function: " + a); //is Person instance of Function object
-
 console.log("is Person instance of Object: " + b);
+
+var c = new Person(1, 'Ricky', '29');
+console.log("Value of c: " + JSON.stringify(c));
+
+var d = Object.create(Person.prototype);
+console.log("Value of d: " + d);
+
+console.log("Prototype of Person: " + JSON.stringify(Person.prototype));
+
+console.log("Values associated with Person object: ");
+for(var i in c) {
+    console.log("i = " + i );
+}
