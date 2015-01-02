83ab1647e714444e2e1d2ef32e758216ba102d89
Prabhash Rathore <prabhashrathore@gmail.com>
2015-01-01 20:51:08 -0800

    determine terminal size window using stdout api

---
+++ NodeCodeSnippet/nodelib/TerminalSize.js
@@ -0,0 +1,21 @@
+/**
+ * Program to determine the terminal size of your program.
+ *
+ * Created by prrathore on 1/1/15.
+ */
+
+function size() {
+    var columns = process.stdout.columns;
+    var rows = process.stdout.rows;
+
+    console.log("Size:  " + columns + "x" + rows);
+}
+
+if (!process.stdout.isTTY) {
+    console.error("Not using a terminal window!");
+    process.exit(-1);
+}
+
+size();
+process.stdout.on("resize", size);
+process.stdin.resume();
