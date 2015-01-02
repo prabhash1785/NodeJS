4ba04417d52cb7d6a4d72fee155ab9289c2c969e
Prabhash Rathore <prabhashrathore@gmail.com>
2015-01-01 20:16:00 -0800

    commander module to parse command line args

--- NodeCodeSnippet/nodelib/CommandLine.js
+++ NodeCodeSnippet/nodelib/CommandLine.js
@@ -9,3 +9,20 @@
 process.argv.forEach(function(val, index) {
     console.log(index + " : " + val);
 });
+
+var typeOfArgv = Array.isArray(process.argv);
+console.log("Is process.argv an Array: " + typeOfArgv); //true
+
+//use commander module
+var commander = require('commander');
+
+commander
+    .option("-f, --foo <i>", "Integer value for foo", parseInt, 15)
+    .option("-b, --bar [j]", "Integer value for bar", parseInt, 10)
+    .option("-z, --baz", "Boolean argument baz")
+    .parse(process.argv);
+
+console.log(commander.foo); //15
+console.log(commander.bar); //10
+console.log(commander.baz); //undefined
+
