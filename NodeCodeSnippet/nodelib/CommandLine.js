437286bab26cd3cdc88bd913950da54e044ac9c9
Prabhash Rathore <prabhashrathore@gmail.com>
2015-01-01 20:39:55 -0800

    use of tty interface to print standard streams tty types

--- NodeCodeSnippet/nodelib/CommandLine.js
+++ NodeCodeSnippet/nodelib/CommandLine.js
@@ -28,6 +28,13 @@ console.log(commander.baz); //undefined
 
 //use of util module
 var util = require('util');
-
 util.log("Node is Awesome!!"); //util.log adds a timestamp to log statements
+
+//use of tty interface to check if streams are associated with terminal window
+//run from command line to see boolean values otherwise it will show undefined
+console.warn("stdin  = " + process.stdin.isTTY);
+console.warn("stdout = " + process.stdout.isTTY);
+console.warn("stderr = " + process.stderr.isTTY);
+
+
 
