1ffae7b358ba46ce10512c4e1ea2c945156cf848
Prabhash Rathore <prabhashrathore@gmail.com>
2015-01-03 01:17:46 -0800

    use of path module to create os based dir path

--- NodeCodeSnippet/nodelib/filesystem/FileSystem.js
+++ NodeCodeSnippet/nodelib/filesystem/FileSystem.js
@@ -4,6 +4,18 @@
  * Created by prrathore on 1/3/15.
  */
 
-//find current path for file
+//find absolute path of current executing file
 console.log("This file is in: " + __filename);
 console.log("This file is in directory: " + __dirname);
+
+console.log('Current directory: ' + process.cwd());
+
+//path module
+var path = require('path');
+
+var dir = ['delhi', 'bangalore', 'chicago'];
+var fullPath = dir.join(path.sep);
+console.log("Full Directory Path: " + fullPath);
+
+
+//fs module
