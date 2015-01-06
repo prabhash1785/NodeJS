7483f1e493b2ac97d59401065cefbb30618ae140
Prabhash Rathore <prabhashrathore@gmail.com>
2015-01-06 14:33:04 -0800

    sample stream based program

---
+++ NodeCodeSnippet/nodelib/streams/StreamSample.js
@@ -0,0 +1,28 @@
+/**
+ * Sample program to use Stream APIs.
+ *
+ * Created by prrathore on 1/6/15.
+ */
+
+var Stream = require('stream');
+var stream = new Stream();
+var duration = 5 * 1000; // 5 seconds
+var end = Date.now() + duration;
+var interval;
+
+stream.readable = true;
+interval = setInterval(function() {
+    var now = Date.now();
+
+    console.log("Emitting a data event");
+    stream.emit("data", new Buffer("foo"));
+
+    if (now >= end) {
+        console.log("Emitting an end event");
+        stream.emit("end");
+        clearInterval(interval);
+    }
+}, 1000);
+
+
+
