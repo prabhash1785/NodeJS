c37eda55de829fa586205d3a8d771df4d64a64bd
Prabhash Rathore <prabhashrathore@gmail.com>
2015-01-11 01:58:48 -0800

    file upload application within express, dust and multer

--- fileupload/app.js
+++ fileupload/app.js
@@ -5,6 +5,13 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var dust = require('dustjs-linkedin');
+var cons = require('consolidate');
+
+var multer = require('multer');
+
+//var dustjs = require('adaro');
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -12,7 +19,14 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//app.set('view engine', 'jade');
+
+app.engine('dust', cons.dust);
+app.set('view engine', 'dust');
+app.set('template_engine', 'dust');
+
+//app.engine('dust', dustjs.dust({ ... });
+//app.set('view engine', 'dust');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -22,39 +36,92 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(multer({
+    dest: '/Users/prrathore/temp/cardfileuploads',
+    limits: {
+        fieldNameSize: 500,
+        files: 2,
+        fields: 5
+    },
+    rename: function (fieldname, filename) {
+        return fieldname + filename + Date.now();
+    },
+    onFileUploadStart: function (file) {
+        console.log('Upload starting for filename: ' + file.originalname);
+    },
+    onFileUploadData: function (file, data) {
+        console.log(data.length + ' of ' + file.fieldname + ' arrived')
+    },
+    onParseStart: function () {
+        console.log('Form parsing started at: ', new Date())
+    },
+    onParseEnd: function (req, next) {
+        console.log('Form parsing completed at: ', new Date());
+
+        // usage example: custom body parse
+        //req.body = require('qs').parse(req.body);
+
+        // call the next middleware
+        next();
+    },
+    onFileUploadComplete: function (file) {
+        console.log(file.fieldname + ' uploaded to  ' + file.path);
+    },
+    onFileSizeLimit: function (file) {
+        console.log('Failed: ', file.originalname)
+        fs.unlink('./' + file.path) // delete the partially written file
+    },
+    onFilesLimit: function () {
+        console.log('Crossed file limit!')
+    },
+    onFieldsLimit: function () {
+        console.log('Crossed fields limit!')
+    },
+    onPartsLimit: function () {
+        console.log('Crossed parts limit!')
+    },
+    onError: function(error, next) {
+        console.log("Error occurred while uploading the file!!");
+        next(error);
+    }
+}));
+
+
 app.use('/', routes);
 app.use('/users', users);
 
+
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
+//app.use(function(req, res, next) {
+//    var err = new Error('Not Found');
+//    err.status = 404;
+//    next(err);
+//});
 
 // error handlers
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
+//if (app.get('env') === 'development') {
+//    app.use(function(err, req, res, next) {
+//        res.status(err.status || 500);
+//        res.render('error', {
+//            message: err.message,
+//            error: err
+//        });
+//    });
+//}
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
+//app.use(function(err, req, res, next) {
+//    res.status(err.status || 500);
+//    res.render('error', {
+//        message: err.message,
+//        error: {}
+//    });
+//});
 
 
 module.exports = app;
--- fileupload/routes/index.js
+++ fileupload/routes/index.js
@@ -1,9 +1,46 @@
 var express = require('express');
 var router = express.Router();
 
+var fs = require('fs');
+var util = require('util');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('datauploadform', { title: 'Upload File' });
+  //res.sendFile('/Users/prrathore/MyGitRepo/NodeJS/fileupload/views/index.html');
+});
+
+//router.post("/upload", function(req, res, next){
+//    if (req.files) {
+//        console.log(util.inspect(req.files));
+//        if (req.files.myFile.size === 0) {
+//            return next(new Error("Hey, first would you select a file?"));
+//        }
+//        fs.exists(req.files.myFile.path, function(exists) {
+//            if(exists) {
+//                res.end("Got your file!");
+//            } else {
+//                res.end("Well, there is no magic for those who don’t believe in it!");
+//            }
+//        });
+//    }
+//});
+
+router.post('/upload', function (req, res) {
+
+    console.log("File Uploaded");
+
+    //model.status = "File Uploaded!!";
+
+    var body = req.body;
+    console.log("File attributes: " + JSON.stringify(body));
+
+    var files = req.files;
+    console.log("Files: " + JSON.stringify(files));
+
+
+    res.render('datauploadform', {status : 'File uploaded!!'});
+
 });
 
 module.exports = router;
