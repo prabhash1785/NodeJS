16f643553fcb97314aed446a810438cb3a89c85d
Prabhash Rathore <prabhashrathore@gmail.com>
2015-01-13 16:15:35 -0800

    Added events to file transfer request

--- fileupload/routes/filetransferwithscp.js
+++ fileupload/routes/filetransferwithscp.js
@@ -14,14 +14,45 @@ client.defaults({
     privateKey: fs.readFileSync('/Users/prrathore/.ssh/id_rsa')
 });
 
-client.upload('/Users/prrathore/temp/DEVELOPMENT-application.properties', '/x/home/prrathore/temp/application-dev-scp.txt', function() {
+client.upload('/Users/prrathore/temp/DEVELOPMENT-application.properties', '/x/home/prrathore/temp/application-dev-scp.txt', function(err) {
+
+    if(err) {
+        console.log("File uoload failed: " + err);
+        client.close();
+    }
+
     console.log("file moved to server successfully!!");
     client.close();
 });
 
+client.on('connect', function() {
+    console.log("Connected!!");
+});
+
+client.on('ready', function() {
+    console.log("Ready for file transfer!!");
+});
+
+client.on('error', function() {
+    console.log("Some error occurred!!");
+});
+
+client.on('end', function() {
+    console.log("Socket connection closed/ended!!");
+});
+
+client.on('mkdir', function(dir) {
+    console.log("Directory created: " + dir);
+});
+
+//No need to print this callback as it has very verbose output when we print buffer
+//client.on('transfer', function(buffer, uploaded, total) {
+//    console.log("Transferred: " + buffer + " : " + uploaded + " : " + total);
+//});
+
 client.on('close', function() {
     console.log("Session closed!!");
-})
+});
 
 
 
