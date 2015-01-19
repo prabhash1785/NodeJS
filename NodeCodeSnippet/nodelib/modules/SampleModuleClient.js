6c591a623e85320db92b3609794900daa0861ea0
Prabhash Rathore <prabhashrathore@gmail.com>
2015-01-18 22:28:02 -0800

    code changes based on unit testing

--- NodeCodeSnippet/nodelib/modules/SampleModuleClient.js
+++ NodeCodeSnippet/nodelib/modules/SampleModuleClient.js
@@ -1,12 +1,12 @@
 /**
- * sampleModule module will listen to events emitted by SampleModule and run the call backs.
+ * SampleModuleClient module will listen to events emitted by SampleModule and run the call backs.
  *
  * Created by prrathore on 1/16/15.
  */
 
 var SampleModule = require('./SampleModule');
 
-sampleModule = new SampleModule({length : 50, breadth : 100});
+sampleModule = new SampleModule({length : 50, breadths : 100});
 
 sampleModule.on('undefined', function() {
     console.log("not an object");
