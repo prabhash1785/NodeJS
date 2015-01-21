7635f3330b99e4d771b0f96bfbcd6589740a2fa0
Prabhash Rathore <prabhashrathore@gmail.com>
2015-01-21 00:09:01 -0800

    file apis

--- NodeCodeSnippet/nodelib/filesystem/FileHandler.js
+++ NodeCodeSnippet/nodelib/filesystem/FileHandler.js
@@ -9,15 +9,17 @@ var path = require('path');
 
 var dirName = __dirname + path.sep + 'temp';
 
+//create directory asynchronously
 fs.mkdir(dirName, function() {
     console.log("Directory is created!!");
-})
+});
 
 var fileName = dirName + path.sep + 'test.txt';
 
+//create file asynchronously, no need to close file in this case as it's automatically closed by writeFile API
 fs.writeFile(fileName, 'Playing with Node FS APIs', {encoding: 'utf8', mode: 438, flag: 'w'}, function(err) {
     if(err) {
         throw err;
     }
     console.log("File is created!!");
-})
+});
