9e926d106ece8777a39d5c3c5f4b87633d93b7ed
Prabhash Rathore <prabhashrathore@gmail.com>
2015-01-25 00:27:08 -0800

    basic grunt task

--- NodeCodeSnippet/Gruntfile.js
+++ NodeCodeSnippet/Gruntfile.js
@@ -27,4 +27,12 @@ module.exports = function(grunt) {
         grunt.log.writeln(this.target + ': ' + this.data);
     });
 
+    grunt.registerTask('foo', 'A sample task that logs stuff.', function(arg1, arg2) {
+        if (arguments.length === 0) {
+            grunt.log.writeln(this.name + ", no args");
+        } else {
+            grunt.log.writeln(this.name + ", " + arg1 + " " + arg2);
+        }
+    });
+
 };
