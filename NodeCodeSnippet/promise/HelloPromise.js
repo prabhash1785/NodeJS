e432c14f4c661a798b35696cdc10c43ebf2bd400
Prabhash Rathore <prabhashrathore@gmail.com>
2015-01-25 23:31:43 -0800

    playing with JS Promise API

---
+++ NodeCodeSnippet/promise/HelloPromise.js
@@ -0,0 +1,17 @@
+/**
+ * Promise Code
+ *
+ * Created by prrathore on 1/25/15.
+ */
+
+//Standard JavaScript Promise API
+//var promise1 = new Promise(function(resolve, reject) {
+//    console.log("Promise is fulfilled");
+//});
+//
+//promise1();
+
+(function(message) {
+    console.log(message);
+    //window.alert(message);
+})("Hi There!!");
