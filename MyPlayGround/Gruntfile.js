4dd00d541b5347119b6d10f89303b9f4de0a5334
Prabhash Rathore <prabhashrathore@gmail.com>
2015-05-24 11:36:06 -0700

    documentation

--- MyPlayGround/Gruntfile.js
+++ MyPlayGround/Gruntfile.js
@@ -6,10 +6,12 @@
 
 var grunt = require('grunt');
 
+//To run: grunt default or grunt
 grunt.registerTask('default', 'default task', function() {
     console.log('I am the default task.');
 });
 
+//To run: grunt hello:<param>
 grunt.registerTask('hello', 'greeting task', function(name) {
    if(!name || !name.length)
         grunt.warn('you need to provide a name');
@@ -17,3 +19,4 @@ grunt.registerTask('hello', 'greeting task', function(name) {
     console.log('hello ' + name + '!');
 
 });
+
