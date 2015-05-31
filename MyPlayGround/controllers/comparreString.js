4a7e241160d3e275767e5a3444a7b1d477f0a55b
Prabhash Rathore <prabhashrathore@gmail.com>
2015-05-30 17:24:52 -0700

    sample module for adding two numbers.. created to test my jshint grunt task

---
+++ MyPlayGround/controllers/comparreString.js
@@ -0,0 +1,15 @@
+/**
+ * Created by prrathore on 5/30/15.
+ */
+
+'use strict';
+
+module.exports = function(a, b) {
+
+    if(a === b) {
+        console.log('Equal');
+    } else {
+        console.log('Unequal');
+    }
+
+};
