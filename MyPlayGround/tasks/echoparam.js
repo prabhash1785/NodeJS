65a8d5a9bd241d19e0de8109421379ca0a73f3c1
Prabhash Rathore <prabhashrathore@gmail.com>
2015-05-30 17:47:36 -0700

    fixed lint errors

--- MyPlayGround/tasks/echoparam.js
+++ MyPlayGround/tasks/echoparam.js
@@ -9,12 +9,12 @@ module.exports = function (grunt) {
     //this task doesn't need a configuration object
     grunt.registerTask('sayhi', 'Say Hi', function(name) {
         grunt.log.writeln('Hi ' + name);
-    })
+    });
 
     //Multitask looks for targets defined in configuration object
-    grunt.registerMultiTask('echo', 'echoes the provided param', function(arg) {
-       //grunt.log.writeln('Arguement is: ' + arg);
+    //Mutitask gets it's data from configuration object
+    grunt.registerMultiTask('echo', 'echoes the provided param', function() {
         grunt.log.writeln(this.target + ' :: ' + this.data);
     });
 
-}
+};
