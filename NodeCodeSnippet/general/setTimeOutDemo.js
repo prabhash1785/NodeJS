d7c7c2a6aee924d91982ffdf6c44a035007f6034
Prabhash Rathore <prabhashrathore@gmail.com>
2015-06-15 16:46:55 -0500

    fixed settimeout api call error

--- NodeCodeSnippet/general/setTimeOutDemo.js
+++ NodeCodeSnippet/general/setTimeOutDemo.js
@@ -6,8 +6,11 @@
 
 function addNumbers(param1, param2) {
     console.log('Pre-sum logging');
-    setTimeout(sum(param1, param2), 4000);
+
+    var timerID = setTimeout(sum, 4000, param1, param2);
+
     console.log('Post sum logging');
+
 }
 
 function sum(a, b) {
