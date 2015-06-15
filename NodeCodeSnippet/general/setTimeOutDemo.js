3b771f279c793c9745cc94c1ead0fd498ef298f5
Prabhash Rathore <prabhashrathore@gmail.com>
2015-06-15 16:29:49 -0500

    settimeout api use

---
+++ NodeCodeSnippet/general/setTimeOutDemo.js
@@ -0,0 +1,19 @@
+/**
+ * Created by prrathore on 6/15/15.
+ */
+
+'use strict';
+
+// use settimeout API
+function addNumbers(param1, param2) {
+    console.log('Pre-sum logging');
+    setTimeout(sum(param1, param2), 4000);
+    console.log('Post sum logging');
+}
+
+function sum(a, b) {
+    console.log('Sum: ' + (parseInt(a)  + parseInt(b)));
+}
+
+addNumbers(20, 40);
+
